37a6fe094f5f686c32ae375f19cde15dec15614b
Maxime RIO <maxime.rio@niwa.co.nz>
2023-10-03 04:11:24 +0000

    renamed utility functions, simplify input functions

--- workflow/utils.py
+++ workflow/utils.py
@@ -4,7 +4,9 @@ import yaml
 from snakemake.io import glob_wildcards
 
 
-def filter_session(resultsdir, subject, session, entity):
+def is_valid_session(resultsdir, subject, session, entity):
+    """assess if a session if valid based on the corresponding QC status file"""
+
     qc_file = Path(
         f"{resultsdir}/bids/derivatives/mriqc/"
         f"/sub-{subject}_ses-{session}_{entity}_qc.yaml"
@@ -16,7 +18,9 @@ def filter_session(resultsdir, subject, session, entity):
     return is_valid
 
 
-def list_sessions(resultsdir):
+def list_valid_sessions(resultsdir):
+    """list all valid sessions from every subjects, based on QC status files"""
+
     patterns = glob_wildcards(
         f"{resultsdir}/bids/derivatives/mriqc/"
         "/sub-{subject}_ses-{session}_{entity}_qc.yaml"
@@ -24,7 +28,7 @@ def list_sessions(resultsdir):
 
     subjects, sessions, entities = [], [], []
     for subject, session, entity in zip(*patterns):
-        if filter_session(resultsdir, subject, session, entity):
+        if is_valid_session(resultsdir, subject, session, entity):
             subjects.append(subject)
             sessions.append(session)
             entities.append(entity)
